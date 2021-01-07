30e5a2b4cb5de4a0e0a23920cfbaa126e2534335
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2021-01-07 17:56:53 +0200

    icons added and starting screen done.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let gameBoardContainer = document.querySelector('.gameboard')
 let gameCell = document.querySelectorAll('.game-cell');
 
-let startNewGameBtn = document.querySelector('.new-game-btn');
+let newGameBtn = document.querySelector('.new-game-btn');
+let startGameBtn = document.querySelector('.start-game-btn');
 
 let messageContainer = document.querySelector('.message-container');
 let message = document.querySelector('.message');
+
+let characters = document.querySelectorAll('.character');
+let markerBtns = document.querySelectorAll('.mark');
+
+let characterSelected = false;
+let markerSelected = false;
 
 let playersTurn = true;
 let roundWon = false;
 
 let gameActive = true;
+
+let playersName = '';
+let playersMark = '';
+let computersMark = '';
 
 class Player {
     constructor(name, marker) {
@@ -18,8 +29,8 @@ class Player {
     }
 }
 
-let player = new Player('human', 'x');
-let computer = new Player('computer', 'o');
+let player;
+let computer;
 
 let gameBoardObject = {
     board: [
@@ -40,15 +51,84 @@ let winningConditions = [
     ['a3', 'b2', 'c1'],
 ];
 
+function makePlayerObjects() {
+    if (playersMark == 'x') {
+        computersMark = 'o';
+    } else {
+        computersMark = 'x';
+    }
 
+    player = new Player(playersName, playersMark);
+    computer = new Player('the bad guy', computersMark);
 
+    console.log('players name is ' + player.name + ' and their mark is ' + player.marker);
+    console.log('computers name is ' + computer.name + ' and his mark is ' + computer.marker);
+}
+
+function selectCharacter(e) {
+    let playersIcon = document.querySelector('#players-icon');
+    let characterId = e.target.id;
+    let character = '';
+
+    switch (characterId) {
+        case 'Beau':
+            character = 'bear';
+            break;
+        case 'Dean':
+            character = 'dinosaur';
+            break;
+        case 'Nelly':
+            character = 'ninja';
+            break;
+        case 'Priscilla':
+            character = 'plant-pot';
+            break;
+
+    }
+    playersIcon.innerHTML = '<img src="/pics/' + character + '.png" id="' + characterId + '">' + characterId;
+
+    characterSelected = true;
+    playersName = characterId;
+
+}
+
+function selectMarker(e) {
+    let markerId = e.target.id;
+    markerSelected = true;
+    playersMark = markerId;
+}
+
+function toggleElementSizeWhenClicked(className) {
+    //Get all the matched Elements
+    let elements = document.querySelectorAll("." + className);
+    //Use an variable to rememeber previous clicked element
+    let prevIndex = -1; // 
+    // Loop over the list
+    elements.forEach(function (item, index) {
+        (function (i) { //  A closure is created
+            item.addEventListener('click', function () {
+                // if any previous element was clicked then transform of that element is none
+                if (prevIndex !== -1) {
+                    elements[prevIndex].style.transform = "none";
+                    elements[prevIndex].style.backgroundColor = 'rgb(127, 157, 255)';
+                }
+                // change transform of current element
+                item.style.transform = "scale(1.3)";
+                item.style.backgroundColor = "#a099f8";
+                // update prevIndex
+                prevIndex = i;
+            })
+        }(index))
+
+    })
+}
 
 function reset() {
     playersTurn = true;
     roundWon = false;
     gameActive = true;
 
-    startNewGameBtn.style.display = 'none';
+    newGameBtn.style.display = 'none';
 
     winningConditions = [
         ['a1', 'a2', 'a3'],
@@ -71,7 +151,7 @@ function reset() {
         cell.innerText = ''
     })
 
-    message.innerText = 'Human starts the game';
+    message.innerText = player.name + ' starts the game';
 
 
     //Logs in the console arrays
@@ -90,14 +170,20 @@ function currentPlayerTurnMessage(playerName) {
     message.innerText = "It's " + playerName + "s turn";
 }
 
-function winningMessage(name) {
+function winningMessage(playersName) {
 
-    if (name == 'human') {
-        message.innerHTML = 'Congratulations! <br> ' + name + " wins!";
+    if (playersName) {
+        message.innerHTML = 'Congratulations! <br> ' + playersName + " wins!";
     } else {
-        message.innerHTML = name + " wins!";
+        message.innerHTML = '<p id="smaller-txt">' + player.name + ' loses. </p>' + computer.name + ' wins!';
     }
 }
+
+function gameEndedInATieMessage() {
+    message.innerHTML = "<p id='smaller-txt'>It's a tie.</p> Game over!"
+}
+
+
 
 function computerSelects() {
     let num = Math.floor(Math.random() * 9);
@@ -144,14 +230,14 @@ function handleComputersChoice(element, id) {
 
     if (element.innerHTML == '') {
         console.log(id + ' element is empty');
-        element.innerHTML = 'o';
-        updateGameBoardCondition(gameBoardObject.board, id, 'o');
-        updateGameBoardCondition(winningConditions, id, 'o');
+        element.innerHTML = computer.marker;
+        updateGameBoardCondition(gameBoardObject.board, id, computer.marker);
+        updateGameBoardCondition(winningConditions, id, computer.marker);
 
-        let hasWon = checkPlayStatus('computer');
+        let hasWon = checkPlayStatus();
 
         if (!hasWon) {
-            currentPlayerTurnMessage('human');
+            currentPlayerTurnMessage(player.name);
             switchPlayersTurn();
         } else {
             return
@@ -164,15 +250,14 @@ function handleComputersChoice(element, id) {
 
 function humansTurn(e) {
     let element = e.target;
-    
+
     //If the element is empty, display on that element currentPlayers marker and change players turn
     if (element.innerText == '') {
         console.log('elements inner text is empty');
         element.innerText = player.marker;
         handlePlayersChoice(element);
-        let hasWon = checkPlayStatus('human');
+        let hasWon = checkPlayStatus(player.name);
         if (!hasWon) {
-            
             switchPlayersTurn();
             return true
         }
@@ -189,7 +274,7 @@ function currentPlayersTurn(e) {
 
         //checks that game is active, and humans turn is over
         if (gameActive && turnIsOver) {
-            currentPlayerTurnMessage('computer');
+            currentPlayerTurnMessage(computer.name);
             setTimeout(function () {
                 computerSelects();
             }, 1500);
@@ -206,6 +291,7 @@ function switchPlayersTurn() {
 }
 
 function checkPlayStatus(player) {
+
     for (let i = 0; i < winningConditions.length; i++) {
         let row = winningConditions[i];
         let a = row[0];
@@ -214,14 +300,46 @@ function checkPlayStatus(player) {
 
         if (a == b && b == c) {
             roundWon = true;
-            gameActive = false;            
+            gameActive = false;
             winningMessage(player);
-            startNewGameBtn.style.display = 'block';
+            newGameBtn.style.display = 'block';
             return true;
         }
     }
 
+    let gameIsADraw = checkIfGameIsDraw();
+
+    if (gameIsADraw) {
+        newGameBtn.style.display = 'block';
+        gameEndedInATieMessage();
+        roundWon = true;
+        gameActive = false;
+    }
     return false;
+}
+
+function onlyMarkers(item) {
+    return item === 'x' || item === 'o';
+}
+
+function checkIfGameIsDraw() {
+    let gameboard = gameBoardObject.board;
+    let gameIsATie = false;
+
+    let row1 = gameboard[0].every(onlyMarkers);
+    let row2 = gameboard[1].every(onlyMarkers);
+    let row3 = gameboard[2].every(onlyMarkers);
+
+    if (row1 && row2 && row3 && !roundWon) {
+        gameIsATie = true;
+    }
+
+    console.log(gameIsATie);
+    for (let i = 0; i < gameboard.length; i++) {
+        console.log(gameboard[i]);
+    }
+    return gameIsATie;
+
 }
 
 function handlePlayersChoice(e) {
@@ -229,18 +347,19 @@ function handlePlayersChoice(e) {
     console.log(elementId);
     let gameboard = gameBoardObject.board;
 
-        //Go through arrays and compare elements id to it. 
+    //Go through arrays and compare elements id to it. 
     //Change arrays elements, so that right player marker is in the array in the right place
     // gameboard object and the bameboard in th escreen now looks the same. 
 
-    updateGameBoardCondition(gameboard, elementId, 'x');
-    updateGameBoardCondition(winningConditions, elementId, 'x');
+    updateGameBoardCondition(gameboard, elementId, player.marker);
+    updateGameBoardCondition(winningConditions, elementId, player.marker);
 
     console.clear();
     for (let i = 0; i < gameboard.length; i++) {
         console.log(gameboard[i]);
     }
 }
+
 
 
 function updateGameBoardCondition(arr, elementId, marker) {
@@ -255,13 +374,60 @@ function updateGameBoardCondition(arr, elementId, marker) {
     }
 }
 
+function startGame() {
+    if (characterSelected && markerSelected) {
+        document.querySelector('.game-container').style.display = 'block';
+        document.querySelector('.starting-screen').style.display = 'none';
+    }
 
-gameBoardContainer.addEventListener('click', function (e) { 
+}
+
+function changeStartButtonAppearance(element) {
+    if (element) {
+        startGameBtn.style.backgroundColor = '#fd9999';
+        startGameBtn.style.color = 'black';
+    }
+}
+
+toggleElementSizeWhenClicked('character');
+toggleElementSizeWhenClicked('mark');
+
+function startingMessage() {
+    message.innerHTML = 'Welcome ' + player.name + '!';
+    setTimeout(function () {
+        message.innerHTML = "<p style='font-size: 15px;'>" + player.name + ", you can start the game. <br> Just click the game board and try to beat the bad guy</p>";
+    }, 2000);
+}
+
+gameBoardContainer.addEventListener('click', function (e) {
     if (gameActive) {
-        currentPlayersTurn(e);        
+        currentPlayersTurn(e);
     }
 })
 
-startNewGameBtn.addEventListener('click', function () {
+newGameBtn.addEventListener('click', function () {
     reset();
+
+})
+
+startGameBtn.addEventListener('click', function () {
+    startGame();
+    makePlayerObjects();
+    startingMessage()
+    console.log(playersName + ' ' + playersMark);
+
+})
+
+characters.forEach(function (character) {
+    character.addEventListener('click', function (e) {
+        selectCharacter(e);
+        changeStartButtonAppearance(markerSelected);
+    })
+})
+
+markerBtns.forEach(function (mark) {
+    mark.addEventListener('click', function (e) {
+        selectMarker(e);
+        changeStartButtonAppearance(characterSelected);
+    })
 })
